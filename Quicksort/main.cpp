1e440b396fbc6974e538999917cd10ce3c282973
GustavoHenriqueMuller <46305883+GustavoHenriqueMuller@users.noreply.github.com>
2020-12-09 16:45:47 -0300

    Initial Commit

---
+++ Quicksort/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <ctime>
+#include <omp.h>
+
+using namespace std;
+
+void printArray(int* input, int length) {
+    cout << "{";
+    for (unsigned int i = 0; i < length; i++) {
+        cout << input[i];
+
+        if (i < length - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "}" << endl;
+}
+
+int* randomArray(unsigned int size, int minNumber, int maxNumber) {
+    int* output = new int[size];
+
+    for (unsigned int i = 0; i < size; i++) {
+        output[i] = minNumber + (rand() % (maxNumber - minNumber));
+    }
+
+    return output;
+}
+
+void quicksort(int* input, int startIndex, int endIndex) {
+    if(endIndex - startIndex > 0) {
+        int pivot = input[(startIndex + endIndex)/2];
+        int i = startIndex;
+        int j = endIndex;
+
+        while(i <= j) {
+            while (input[i] < pivot) {
+                i++;
+            }
+
+            while (input[j] > pivot) {
+                j--;
+            }
+
+            if(i <= j) {
+                swap(input[i], input[j]);
+                i++; j--;
+            }
+        }
+
+        if(j > startIndex) {
+            quicksort(input, startIndex, j);
+        }
+
+        if(i < endIndex) {
+            quicksort(input, i, endIndex);
+        }
+    }
+}
+
+int main() {
+    const int arrayLength = 1000000000;
+    const bool print = false;
+    srand(time(NULL));
+
+    // Input
+    int* array = randomArray(arrayLength, 1, 1000);
+    if(print) {
+        cout << "Input: ";
+        printArray(array, arrayLength);
+    }
+
+    // Algorithm
+    auto start = omp_get_wtime();
+    quicksort(array, 0, arrayLength-1);
+    auto end = omp_get_wtime();
+
+    // Output
+    if(print) {
+        cout << "Output: ";
+        printArray(array, arrayLength);
+    }
+
+    cout << "Time: " << end - start << "s" << endl;
+    return 0;
+}
---
+++ QuicksortThreaded/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <ctime>
+#include <omp.h>
+#include <teste.h>
+
+using namespace std;
+
+void printArray(int* input, int length) {
+    cout << "{";
+    for (unsigned int i = 0; i < length; i++) {
+        cout << input[i];
+
+        if (i < length - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "}" << endl;
+}
+
+int* randomArray(unsigned int length, int minNumber, int maxNumber) {
+    int* output = new int[length];
+
+    for (unsigned int i = 0; i < length; i++) {
+        output[i] = minNumber + (rand() % (maxNumber - minNumber));
+    }
+
+    return output;
+}
+
+void quicksort(int* input, int startIndex, int endIndex, const int &minSizeToParallelize) {
+    if(endIndex - startIndex > 0) {
+        int pivot = input[(startIndex + endIndex)/2];
+        int i = startIndex;
+        int j = endIndex;
+
+        while(i <= j) {
+            while (input[i] < pivot) {
+                i++;
+            }
+
+            while (input[j] > pivot) {
+                j--;
+            }
+
+            if(i <= j) {
+                swap(input[i], input[j]);
+                i++;
+                j--;
+            }
+        }
+
+        if(endIndex - startIndex >= minSizeToParallelize){
+            if(j > startIndex) {
+                #pragma omp task
+                {
+                    quicksort(input, startIndex, j, minSizeToParallelize);
+                }
+            }
+
+            if(i < endIndex) {
+                #pragma omp task
+                {
+                    quicksort(input, i, endIndex, minSizeToParallelize);
+                }
+            }
+        } else {
+            if(j > startIndex) {
+                quicksort(input, startIndex, j, minSizeToParallelize);
+            }
+
+            if(i < endIndex) {
+                quicksort(input, i, endIndex, minSizeToParallelize);
+            }
+        }
+    }
+}
+
+int main() {
+    const int minSizeToParallelize = 1000;
+    const int arrayLength = 1000000000;
+    const bool print = false;
+    srand(time(NULL));
+
+    // Input
+    int* array = randomArray(arrayLength, 1, 1000);
+    if(print) {
+        cout << "Input: ";
+        printArray(array, arrayLength);
+    }
+
+    // Algorithm
+    auto start = omp_get_wtime();
+    #pragma omp parallel num_threads(12)
+    {
+        #pragma omp single
+        {
+            quicksort(array, 0, arrayLength-1, minSizeToParallelize);
+        }
+    }
+    auto end = omp_get_wtime();
+
+    // Output
+    if(print) {
+        cout << "Output: ";
+        printArray(array, arrayLength);
+    }
+
+    cout << "Time: " << end - start << "s" << endl;
+
+    return 0;
+}
